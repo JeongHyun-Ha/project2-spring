f80f5d3c4ca9d25ea103f75708fc9897530a9d8f
JeongHyun Ha <aszx2024@naver.com>
2024-05-31 11:30:04 +0900

    댓글 작성 시간 보기 수정

--- src/main/java/com/prj2/domain/comment/Comment.java
+++ src/main/java/com/prj2/domain/comment/Comment.java
@@ -3,6 +3,7 @@ package com.prj2.domain.comment;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class Comment {
@@ -13,4 +14,15 @@ public class Comment {
     private LocalDateTime inserted;
 
     private String nickName;
+
+    public String getInserted() {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+        if (inserted.isBefore(beforeOneDay)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+            return inserted.format(formatter).toString();
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh시 mm분");
+            return inserted.format(formatter).toString();
+        }
+    }
 }
